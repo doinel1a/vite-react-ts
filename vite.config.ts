e4f958373de064755017f05d7fdd53c3da503f30
doinel1a <doinel1atanasiu@gmail.com>
2026-04-27 16:45:16 +0200

    chore: update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,17 @@
 import path from 'node:path';
 
-import { partytownVite } from '@builder.io/partytown/utils';
-import react from '@vitejs/plugin-react-swc';
+import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      "~": path.resolve(__dirname, "."),
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   plugins: [
-    react(),
-    partytownVite({
-      dest: path.join(__dirname, 'dist', '~partytown')
-    })
+    react()
   ]
 });
